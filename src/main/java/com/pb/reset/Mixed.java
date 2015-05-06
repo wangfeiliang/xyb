16aad941d3abcc23d910d55cb2fe15f22139af14
wangfeiliang <wangfeiliang@gintong.com>
2015-05-06 10:35:42 +0800

    ddd

--- src/main/java/com/pb/reset/Mixed.java
+++ src/main/java/com/pb/reset/Mixed.java
@@ -9,6 +9,8 @@ public class Mixed {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
              System.out.println("Mixed");
+             
+             System.out.println("Mixed");System.out.println("Mixed");
 	}
 
 }
